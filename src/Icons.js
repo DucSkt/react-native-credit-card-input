63740731d8cd56cfcaa176e6f63217c0585310d7
hoang van duc <duc.hoangvan@inapps.net>
2021-07-03 11:26:07 +0700

    add new icon

--- src/Icons.js
+++ src/Icons.js
@@ -2,12 +2,16 @@ const Icons = {
   cvc: require("./icons/stp_card_cvc.png"),
   cvc_amex: require("./icons/stp_card_cvc_amex.png"),
   "american-express": require("./icons/stp_card_amex.png"),
+  "amex": require("./icons/stp_card_amex.png"),
   "diners-club": require("./icons/stp_card_diners.png"),
+  "diners": require("./icons/stp_card_diners.png"),
   "master-card": require("./icons/stp_card_mastercard.png"),
+  "mastercard": require("./icons/stp_card_mastercard.png"),
   discover: require("./icons/stp_card_discover.png"),
   jcb: require("./icons/stp_card_jcb.png"),
   placeholder: require("./icons/stp_card_unknown.png"),
   visa: require("./icons/stp_card_visa.png"),
+  unionpay: require("./icons/unionpay.png"),
 };
 
 export default Icons;
